884e5b7c51e53fa3300e3ac21817f35f314777b6
programmerDH-github <programmer_h@naver.com>
2021-11-07 22:21:22 +0900

    ninth

---
+++ src/Baek_Joon/BJ_4458.java
@@ -0,0 +1,31 @@
+package Baek_Joon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_4458 {
+
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		try {
+			int n = Integer.parseInt(bf.readLine());
+			String[] logs = new String[n];
+
+			for (int i = 0; i < n; i++) {
+				logs[i] = bf.readLine().trim();
+			}
+			for (int i = 0; i < n; i++) {
+				System.out.println(logs[i].substring(0, 1).toUpperCase() + logs[i].substring(1));
+			}
+
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
